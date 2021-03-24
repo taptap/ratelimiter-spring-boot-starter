7b0dde45b70b5ab56b76d1baf529189458baddca
kl <chenkailing@xd.com>
2021-03-24 10:25:08 +0800

    触发限流后的响应类型改成 application/json

--- src/main/java/com/taptap/ratelimiter/web/RateLimitExceptionHandler.java
+++ src/main/java/com/taptap/ratelimiter/web/RateLimitExceptionHandler.java
@@ -3,21 +3,22 @@ package com.taptap.ratelimiter.web;
 import com.taptap.ratelimiter.configuration.RateLimiterProperties;
 import com.taptap.ratelimiter.exception.RateLimitException;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import javax.servlet.http.HttpServletResponse;
 
 /**
  * @author kl (http://kailing.pub)
  * @since 2021/3/17
  */
 @ControllerAdvice
-@ConditionalOnProperty(prefix = RateLimiterProperties.PREFIX, name = "exceptionHandler.enable", havingValue = "true",matchIfMissing = true)
+@ConditionalOnProperty(prefix = RateLimiterProperties.PREFIX, name = "exceptionHandler.enable", havingValue = "true", matchIfMissing = true)
 public class RateLimitExceptionHandler {
 
-    private final  RateLimiterProperties limiterProperties;
+    private final RateLimiterProperties limiterProperties;
 
     public RateLimitExceptionHandler(RateLimiterProperties limiterProperties) {
         this.limiterProperties = limiterProperties;
@@ -25,9 +26,11 @@ public class RateLimitExceptionHandler {
 
     @ExceptionHandler(value = RateLimitException.class)
     @ResponseBody
-    public String exceptionHandler(HttpServletResponse response, RateLimitException e){
-        response.setStatus(limiterProperties.getStatusCode());
-        response.setHeader("Retry-After", String.valueOf(e.getRetryAfter()));
-        return limiterProperties.getResponseBody();
+    public ResponseEntity<String> exceptionHandler(RateLimitException e) {
+
+        return ResponseEntity.status(limiterProperties.getStatusCode())
+                .header(HttpHeaders.RETRY_AFTER, String.valueOf(e.getRetryAfter()))
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(limiterProperties.getResponseBody());
     }
 }
